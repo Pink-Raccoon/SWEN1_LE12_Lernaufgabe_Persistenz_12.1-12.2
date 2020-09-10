631a85cf2183266a998628f4fa0a91dd0cd68a9e
bacn <mbachmann@trdi.io>
2020-09-10 17:24:40 +0200

    introduce unit test order

--- src/test/java/com/example/employees/dao/TestEmployeeDaoImpl.java
+++ src/test/java/com/example/employees/dao/TestEmployeeDaoImpl.java
@@ -5,32 +5,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.sql.SQLException;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import com.example.employees.domain.Employee;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestEmployeeDaoImpl {
 
 	private EmployeeDAOImpl employeeDAO;
+	private static long testEmployeeId;
 
 	@BeforeEach
 	public void setUp() {
 		this.employeeDAO = new EmployeeDAOImpl();
 	}
 
-
+	@Order(1)
 	@Test
 	public void shouldReturnSixRecords() throws SQLException {
 		assertEquals(6, employeeDAO.getAllEmployees().size());
 	}
 
+	@Order(2)
 	@Test
 	public void shouldRetrieveNewEmployee()  throws SQLException {
 		int size= employeeDAO.getAllEmployees().size();
 		Employee testEmp = new Employee("Simpson", "Williams", "1-01-1985", "Unemployed", "Finance", "simpson.williams@abc.com");
-		long testEmployeeId = employeeDAO.addEmployee(testEmp);
+		testEmployeeId = employeeDAO.addEmployee(testEmp);
 		Assert.assertEquals(testEmployeeId, size + 1);
 		testEmp.setId(testEmployeeId);
 		Employee retrievedEmployee = employeeDAO.getEmployeeById(testEmployeeId);
@@ -38,4 +39,10 @@ public class TestEmployeeDaoImpl {
 	}
 
 
+	@Order(5)
+	@Test
+	public void shouldDeleteEmployee() {
+		employeeDAO.removeEmployee(testEmployeeId);
+		Assert.assertEquals(6, employeeDAO.getAllEmployees().size());
+	}
 }
